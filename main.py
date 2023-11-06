4684f6b3eadbe786075f6d1b8095343a575a23ea
David Moreno Fernandez <dmoreno2@seidor.es>
2023-11-06 13:39:00 +0100

    3 almost done

--- main.py
+++ main.py
@@ -16,9 +16,23 @@ def importar_archivos_nparray(archivo):
 
 def crear_grafico_tm(dades_np):
     filas_tm = np.array(dades_np[dades_np[:, 3] == 'TM'])
-    resultado = filas_tm[filas_tm[:, 0].__contains__("2022-02")]
+    resultado = []
+    for i in filas_tm:
+        if '2022-02' in i[0]:
+            resultado.append(i)
+    resultado = np.array(resultado)
+    dates= []
+    D5 = []
     for i in resultado:
-        print(i)
+        if (i[0] not in dates):
+            dates.append(i[0])
+        if i[2] == 'D5' :
+            D5.append(i)
+
+    dates = np.array(dates)
+    D5np= D5
+    
+ 
 
 
     lista_estaciones = []
@@ -26,19 +40,8 @@ def crear_grafico_tm(dades_np):
     lista_estaciones.append(filas_tm[filas_tm[:, 2] == 'X2'])
     lista_estaciones.append(filas_tm[filas_tm[:, 2] == 'X4'])
     lista_estaciones.append(filas_tm[filas_tm[:, 2] == 'X8'])
-    lista_estaciones.append(filas_tm[filas_tm[:, 2] == 'D6'])
     
-    for i in range(0, 4):
-        colores = ["b" , "r", "w", "g"]
-        # Obtiene las fechas y valores de temperatura media
-        fechas = np.array([np.datetime64(fecha) for fecha in lista_estaciones[i][:, 0]])
-        temperatura_media = lista_estaciones[i][:, 4].astype(float)
-
-        # Crea el gráfico
-        plt.figure(figsize=(12, 6))
-
-        # Plotea los datos de temperatura media
-        plt.plot(fechas, temperatura_media, label='Temperatura Media (TM)', color=colores[i])
+    plt.plot(dates, D5, label='Temperatura Media (TM)', color= "w")
 
 
     # Personaliza la gráfica
