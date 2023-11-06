b096ba10f7cf0024fd8a37b4e1c429c71ef5a67f
sjo <usuari@domini.com>
2023-11-06 20:02:25 +0100

    3 done

--- main.py
+++ main.py
@@ -23,46 +23,68 @@ def crear_grafico_tm(dades_np):
     resultado = np.array(resultado)
     dates= []
     D5 = []
+    X2 = []
+    X4 = []
+    X8 = []
     for i in resultado:
         if (i[0] not in dates):
             dates.append(i[0])
-        if i[2] == 'D5' :
-            D5.append(i)
+        match i[2]:
+            case'D5' :
+                D5.append(i[4])
+            case'X2' :
+                X2.append(i[4])
+            case'X4' :
+                X4.append(i[4])
+            case'X8' :
+                X8.append(i[4])
 
-    dates = np.array(dates)
-    D5np= D5
+  
+  
     
- 
+    # plt.plot(dates, D5, label="D5", color= "r")
+    # plt.plot(dates, X2, label="X2", color= "y")
+    # plt.plot(dates, X4, label="X4", color= "b")
+    # plt.plot(dates, X8, label="X8", color= "g")
 
+    # plt.xlabel('Fechas')
+    # plt.ylabel('Temperatura Media (°C)')
+    # plt.title('Comparativa de Temperatura Media Diaria - Febrero 2022')
+    # plt.legend()
 
-    lista_estaciones = []
-    lista_estaciones.append(filas_tm[filas_tm[:, 2] == 'D5'])
-    lista_estaciones.append(filas_tm[filas_tm[:, 2] == 'X2'])
-    lista_estaciones.append(filas_tm[filas_tm[:, 2] == 'X4'])
-    lista_estaciones.append(filas_tm[filas_tm[:, 2] == 'X8'])
-    
-    plt.plot(dates, D5, label='Temperatura Media (TM)', color= "w")
+    # plt.xticks(rotation=60)
+    # plt.grid(True)
 
+    # plt.show()
 
-    # Personaliza la gráfica
-    plt.xlabel('Fechas')
-    plt.ylabel('Temperatura Media (°C)')
-    plt.title('Comparativa de Temperatura Media Diaria - Febrero 2022')
-    plt.legend()
-
-    plt.xticks(rotation=45)
-    plt.grid(True)
-
-    # Muestra la gráfica
+    plt.subplot(1,4,1)
+    plt.plot(dates, D5, label="D5", color= "r")
+    plt.xticks(rotation=90)
+    plt.subplot(1,4,2)
+    plt.plot(dates, X2, label="X2", color= "y")
+    plt.xticks(rotation=90)
+    plt.subplot(1,4,3)
+    plt.plot(dates, X4, label="X4", color= "b")
+    plt.xticks(rotation=90)
+    plt.subplot(1,4,4)
+    plt.plot(dates, X8, label="X8", color= "g")
+    plt.xticks(rotation=90)
     plt.show()
+
+    return [D5, X2, X4, X8]
+
+def calcula_temp_2023(temps_2022):
+    D5 = temps_2022[0]
+    X2 = temps_2022[1]
+    X4 = temps_2022[2]
+    X8 = temps_2022[3]
 
 
 def main():
     estacions = importar_archivos_nparray("2020_MeteoCat_Estacions.csv")
     detall_estacions = importar_archivos_nparray("2022_MeteoCat_Detall_Estacions.csv")
     metadades = importar_archivos_nparray("MeteoCat_Metadades.csv")
-
-    crear_grafico_tm(detall_estacions)
-
+    temp_mitj_2022_estacions = crear_grafico_tm(detall_estacions)
+    calcula_temp_2023(temp_mitj_2022_estacions)
 
 main()
